3cba5d9652627256939fd9dfc4196b607e220011
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-03-21 15:49:37 -0400

    Improved file reading.

--- mesh/particle.go
+++ mesh/particle.go
@@ -1,6 +1,7 @@
 package mesh
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/npadmana/go-xi/utils"
 	"io"
@@ -16,9 +17,37 @@ func (p *Particle) String() string {
 	return fmt.Sprintf("(%g,%g,%g),%g", p.X[0], p.X[1], p.X[2], p.W)
 }
 
+func countlines(fn string) (n int, err error) {
+	n = 0
+	ff, err := os.Open(fn)
+	if err != nil {
+		return 0, err
+	}
+	defer ff.Close()
+	fbuf := bufio.NewReader(ff)
+
+	err = nil
+	for err == nil {
+		n += 1
+		_, err = fbuf.ReadString('\n')
+	}
+	if err != io.EOF {
+		return 0, err
+	}
+	n -= 1
+	return n, nil
+}
+
 func ReadParticles(fn string) ([]Particle, error) {
-	var parr []Particle
-	var p1 *Particle
+	// Get the number of lines and allocate
+	nlines, err := countlines(fn)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Expect to get nlines =", nlines)
+
+	// Allocate particle data
+	parr := make([]Particle, nlines)
 
 	// Open file
 	ff, err := os.Open(fn)
@@ -26,17 +55,15 @@ func ReadParticles(fn string) ([]Particle, error) {
 		return nil, err
 	}
 	defer ff.Close()
+	fbuf := bufio.NewReader(ff)
 
 	// Read loop
-	for err == nil {
-		p1 = new(Particle)
-		_, err = fmt.Fscan(ff, &p1.X[0], &p1.X[1], &p1.X[2], &p1.W)
-		if err == nil {
-			parr = append(parr, *p1)
+	for ii := 0; ii < nlines; ii++ {
+		_, err = fmt.Fscan(fbuf, &parr[ii].X[0], &parr[ii].X[1], &parr[ii].X[2], &parr[ii].W)
+		if err != nil {
+			return nil, err
 		}
 	}
-	if err != io.EOF {
-		return nil,err
-	}
+
 	return parr, nil
 }
